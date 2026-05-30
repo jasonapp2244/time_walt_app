bb1e3e830f61de82bb3c0462a8341a0019d623ca
jasonapp2244 <jasonparker2244@gmail.com>
2026-05-30 05:23:40 +0500

    Add missing Log import to ProfileController

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -9,6 +9,7 @@ use App\Http\Requests\Profile\UpdateTimezoneRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
